574694c144574ad62b2384f8257e19ed19a9393f
Alejandro Patiño <alexpatino92@yahoo.com>
2024-05-22 12:14:21 +0200

    ejercicios array

--- EjArray.kt
+++ EjArray.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 
 fun main(){
     /*Crear 2 arrays de enteros
@@ -19,5 +20,70 @@ println("Array Impares")
     }
 
     println(misImPares.contentToString())
+
+
+println()
+
+/*Programa que genera 10 numeros */
+
+
+
+/*Programa que genera un array de x numeros aleatorios entre 1 y 1000
+Muestra los valores por pantalla y calcula el maximo, minimo y la media*/
+var x=100
+val random= Array<Int>(x){(1..1000).random()}
+var maxValor= random[0]
+var minValor= random[0]
+var suma =0
+
+
+
+for(num in random){
+    print("$num, ")
+    if(num>maxValor){
+        maxValor=num
+    }
+
+    if(num<minValor){
+        minValor=num
+    }
+    suma= num + suma
+}
+
+var media = suma/x;
+
+println("el valor maximo es: $maxValor")
+println("el valor minimo es $minValor")
+println("media del array: $media")
+
+
+/*Programa que genera un array de x numeros aleatorios entre 1 y 1000
+Ordena los elementos del array de menor a mayor
+Imprime los valores del array del inicio al final
+Imprime los valores del array del final al inicio
+*/
+
+val aleatorio= Array<Int>(x){(1..1000).random()}
+//Ordenamos array
+aleatorio.sort()
+println("Inicio a final")
+println(aleatorio.contentToString())
+println("final a inicio")
+println(aleatorio.reversedArray().joinToString("-"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
